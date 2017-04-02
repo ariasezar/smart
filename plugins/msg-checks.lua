7100ba508c644762553e52ade5a137fc823b92e5
ariasezar <asghar.ghasemi.96@gmail.com>
2017-04-02 20:59:35 +0430

    aria

--- plugins/msg-checks.lua
+++ plugins/msg-checks.lua
@@ -1,4 +1,4 @@
---Begin msg_checks.lua @mohammadrezajiji
+--Begin msg_checks.lua @SsS_aria_SsS
 local TIME_CHECK = 2
 local function pre_process(msg)
 local data = load_data(_config.moderation.data)
